7f43485ae7177984e1e4bf1d0535bae5bd260d56
chunho2000 <kokchunho@gmail.com>
2016-12-04 21:50:31 +0800

    pet simulation game
    first commit

---
+++ pet pet/Assets/scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject flashtext;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("flashTheText", 0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp(0))
+			SceneManager.LoadScene ("game");
+	}
+	void flashTheText() {
+		if (flashtext.activeInHierarchy)
+			flashtext.SetActive (false);
+		else
+			flashtext.SetActive (true);
+
+	}
+}
